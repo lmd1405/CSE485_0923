3f28dd37e51647dac777d0eb939bce1241618e0b
lmd1405 <144314725+lmd1405@users.noreply.github.com>
2023-10-03 19:08:34 +0700

    Delete BTTH01_CSE485_ex01 directory

--- BTTH01_CSE485_ex01/BT7.php
+++
@@ -1,9 +0,0 @@
-<?php
- $array = [12, 5, 200, 10, 125, 60, 90, 345, -123, 100, -125, 0];
- echo"các số từ 100 đến 200 và chia hết cho 5: ";
- foreach($array as $val){
-    if($val>=100&&$val<=200&&$val%5==0){
-        echo $val." ";
-    }
- }
- ?>
